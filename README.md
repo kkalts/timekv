1dc1281ff39d1a5f4b607048b899506d2ced4842
kkalts <2784527926@qq.com>
2022-08-19 00:31:50 +0800

    [修改][跳表简单实现 未验证][123]

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# corekv
+# timekv
 
-corekv 是一个用来高效率的验证kv引擎feature的项目。
+timekv是corekv的fork 是一个用来高效率的验证kv引擎feature的项目。
--- utils/slice.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-// Slice holds a reusable buf, will reallocate if you request a larger size than ever before.
-// One problem is with n distinct sizes in random order it'll reallocate log(n) times.
-type Slice struct {
-	buf []byte
-}
